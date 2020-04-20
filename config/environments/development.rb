89c68e7e6a39b97df1c0a344387429a7c052bd24
cbonamin <cbonamin@gmail.com>
2020-04-20 10:56:19 -0300

    initialize users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,21 +64,21 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
 end
-
-Depot_2::Application.configure do
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:
-  "smtp.gmail.com",
-  port:
-  587,
-  domain:
-  "domain.of.sender.net",
-  authentication: "plain",
-  user_name:
-  "dave",
-  password:
-  "secret",
-  enable_starttls_auto: true
-  }
-  end
+# config for Gmail -->
+#Depot_2::Application.configure do
+ # config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+#  address:
+#  "smtp.gmail.com",
+# port:
+#  587,
+#  domain:
+# "domain.of.sender.net",
+# authentication: "plain",
+# user_name:
+#  "dave",
+#  password:
+# "secret",
+ # enable_starttls_auto: true
+  #}
+  #end
